76d7cd81b04e314d62377e085d66b45a099dd398
YuriySamusew <6177385@gmail.com>
2022-09-10 19:29:20 +0300

    Task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,27 @@
+const formRef = document.querySelector('.login-form');
+
+formRef.addEventListener('submit', handleSubmit);
+
+function handleSubmit (event) {
+    event.preventDefault();
+
+    const { elements: { email, password } 
+} = event.currentTarget;
+
+    if (email.value === '' || password.value === '') {
+        return alert('Все поля должны быть заполнены!')
+    }
+    const formData = new FormData(event.currentTarget);
+    // console.log(formData);
+
+    formData.forEach((value, name) => {
+        console.log('handleSubmit -> name', name);
+        console.log('handleSubmit -> value', value);
+    })
+
+    // console.log(`Email: ${email.value}, Password: ${password.value}`);
+    
+    event.currentTarget.reset();
+}
+
 
