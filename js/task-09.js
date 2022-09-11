4e9b0481e0c919152ab1f0ed75a1c8652a683d1c
YuriySamusew <6177385@gmail.com>
2022-09-11 13:30:56 +0300

    Task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,29 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+// const btnChangeColorRef = document.querySelector('.change-color');
+// const spanColorEl = document.querySelector('.color');
+// const changeBodyEl = document.querySelector('body');
+
+// btnChangeColorRef.addEventListener('click', changeColor);
+
+// function changeColor () {
+//   const currentColor = getRandomHexColor();
+//   spanColorEl.textContent = currentColor;
+//   changeBodyEl.style.backgroundColor = currentColor;
+// }
+
+const refs = {
+  btnChangeColorRef: document.querySelector('.change-color'),
+  spanColorEl: document.querySelector('.color'),
+  changeBodyEl: document.querySelector('body'),
+};
+
+refs.btnChangeColorRef.addEventListener('click', changeColor);
+
+function changeColor () {
+    const currentColor = getRandomHexColor();
+    refs.spanColorEl.textContent = currentColor;
+    refs.changeBodyEl.style.backgroundColor = currentColor;
+  }
